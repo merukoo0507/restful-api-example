af090f22bad51b56d8e0c06579d1f2cec6e83f91
Rea Yen <reayen@qnap.com>
2020-08-03 17:13:59 +0800

    add readme

--- app/src/main/java/com/example/mvvm_sample/util/Constants.kt
+++ app/src/main/java/com/example/mvvm_sample/util/Constants.kt
@@ -2,6 +2,5 @@ package com.example.mvvm_sample.util
 
 object Constants {
     const val USER_LINIT = 20
-    const val DEFAULT_NAME = "merukoo0507"
     const val token = "0d8a30826aedc5d3462a32544720459c1207eecd"
 }
